44ec6e72ab98f20b2aeec52fdbaf01e9f1095c8c
jcarter <jcarter62@gmail.com>
2020-01-29 17:34:16 -0800

    work on parcel list

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, make_response
+from flask_bootstrap import Bootstrap
 from data import Parcels
 import json
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    context = {
+        'title': 'home'
+    }
+    return render_template('home.html', context=context)
 
 
 @app.route('/test')
@@ -16,6 +21,13 @@ def route_load():
     p.load_parcels()
     p.filter_parcels('011')
     return json.dumps(p.parcels_flt), 200
+
+
+@app.route('/api/parcels')
+def route_api_parcels():
+    p = Parcels()
+    p.load_parcels()
+    return jsonify(p.parcels), 200
 
 
 if __name__ == '__main__':
